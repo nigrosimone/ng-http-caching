2410552e0dd14b3327a5c8c2350bbe50900fc20b
Nigro Simone <nigro.simone@gmail.com>
2020-08-06 12:20:44 +0200

    draft

--- publish.bat
+++ publish.bat
@@ -1,3 +1,12 @@
 @echo off
-npm run build ng-http-caching --prod && copy /y "%cd%\README.md" "%cd%\dist\ng-http-caching\README.md" && copy /y "%cd%\LICENSE" "%cd%\dist\ng-http-caching\LICENSE" && cd dist/ng-http-caching && npm publish --ignore-scripts
+npm version patch && ^
+cd "%cd%\projects\ng-http-caching" && ^ 
+npm version patch && ^
+cd "%cd%" && ^ 
+npm run build ng-http-caching --prod && ^
+copy /y "%cd%\README.md" "%cd%\dist\ng-http-caching\README.md" && ^
+copy /y "%cd%\LICENSE" "%cd%\dist\ng-http-caching\LICENSE" && ^
+cd "%cd%\dist\ng-http-caching" && ^
+npm publish --ignore-scripts && ^
+cd "%cd%"
 pause

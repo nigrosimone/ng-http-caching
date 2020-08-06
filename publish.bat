25f0868c76d7a141e5f0bdbe6e4ce9c683e428d3
Nigro Simone <nigro.simone@gmail.com>
2020-08-06 12:21:54 +0200

    up

--- publish.bat
+++ publish.bat
@@ -1,8 +1,8 @@
 @echo off
 npm version patch && ^
-cd "%cd%\projects\ng-http-caching" && ^ 
+cd "%cd%\projects\ng-http-caching" && ^
 npm version patch && ^
-cd "%cd%" && ^ 
+cd "%cd%" && ^
 npm run build ng-http-caching --prod && ^
 copy /y "%cd%\README.md" "%cd%\dist\ng-http-caching\README.md" && ^
 copy /y "%cd%\LICENSE" "%cd%\dist\ng-http-caching\LICENSE" && ^

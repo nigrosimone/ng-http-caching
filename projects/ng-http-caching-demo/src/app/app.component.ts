18e25baa8f3edf2d2bfc7689f9cd07dbcdb8d9bd
Nigro Simone <nigro.simone@gmail.com>
2020-08-09 14:25:25 +0200

    demo

--- projects/ng-http-caching-demo/src/app/app.component.ts
+++ projects/ng-http-caching-demo/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { NgHttpCachingService, NgHttpCachingHeaders, NgHttpCachingConfig} from '../../../ng-http-caching/src/lib/ng-http-caching.service';
+import {
+  NgHttpCachingService,
+  NgHttpCachingHeaders,
+  NgHttpCachingConfig,
+} from '../../../ng-http-caching/src/lib/ng-http-caching.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   public url = 'https://my-json-server.typicode.com/typicode/demo/db';
@@ -15,46 +18,33 @@ export class AppComponent {
   public cachedKeys: any = [];
   public timeSpan: number = null;
   public nocache = false;
-  public filetime = '3600';
+  public filetime = null;
 
   private config: NgHttpCachingConfig;
 
   constructor(
     private ngHttpCachingService: NgHttpCachingService,
-    private http: HttpClient){
-      this.config = this.ngHttpCachingService.getConfig();
-      this.filetime = this.config.lifetime.toString();
-    }
-
-    updateCachedKeys(): void {
-      const keys = [];
-      this.ngHttpCachingService.store.forEach((value, key) => {
-        const headers = [];
-        Object.values(NgHttpCachingHeaders).forEach(ngHttpCachingHeaders => {
-          if ( value.request.headers.has(ngHttpCachingHeaders) ) {
-            headers.push({[ngHttpCachingHeaders]: value.request.headers.get(ngHttpCachingHeaders)});
-          }
-        });
-        keys.push({key, headers});
-      });
-      this.cachedKeys = keys;
-    }
+    private http: HttpClient
+  ) {
+    this.config = this.ngHttpCachingService.getConfig();
+    this.filetime = null;
+  }
 
   getRequest(): void {
     this.timeSpan = null;
     const timeStart = new Date();
 
     let headers = new HttpHeaders();
-    if ( this.tag ) {
+    if (this.tag) {
       headers = headers.set(NgHttpCachingHeaders.TAG, this.tag);
     }
-    if ( this.nocache ) {
+    if (this.nocache) {
       headers = headers.set(NgHttpCachingHeaders.DISALLOW_CACHE, '1');
     }
-    if ( Number(this.filetime) !== this.config.lifetime ) {
+    if (this.filetime && Number(this.filetime) !== this.config.lifetime) {
       headers = headers.set(NgHttpCachingHeaders.LIFETIME, this.filetime);
     }
-    this.http.get(this.url, { headers }).subscribe(result => {
+    this.http.get(this.url, { headers }).subscribe((result) => {
       this.timeSpan = new Date().getTime() - timeStart.getTime();
       this.updateCachedKeys();
       console.log('response', result);
@@ -74,5 +64,28 @@ export class AppComponent {
   clearCacheByRegex(): void {
     this.ngHttpCachingService.clearCacheByRegex(new RegExp(this.regex));
     this.updateCachedKeys();
+  }
+
+  clearCacheByKey(): void {
+    this.ngHttpCachingService.clearCacheByKey(this.url);
+    this.updateCachedKeys();
+  }
+
+  updateCachedKeys(): void {
+    const keys = [];
+    this.ngHttpCachingService.store.forEach((value, key) => {
+      const headers = [];
+      Object.values(NgHttpCachingHeaders).forEach((ngHttpCachingHeaders) => {
+        if (value.request.headers.has(ngHttpCachingHeaders)) {
+          headers.push({
+            [ngHttpCachingHeaders]: value.request.headers.get(
+              ngHttpCachingHeaders
+            ),
+          });
+        }
+      });
+      keys.push({ key, headers });
+    });
+    this.cachedKeys = keys;
   }
 }

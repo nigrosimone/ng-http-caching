5e31996712f7d8637d049cc13f64b7feba36546d
Nigro Simone <nigro.simone@gmail.com>
2024-01-03 12:52:15 +0100

    removed implementation

--- projects/ng-http-caching/src/lib/storage/ng-http-caching-memory-storage.ts
+++ projects/ng-http-caching/src/lib/storage/ng-http-caching-memory-storage.ts
@@ -1,35 +1,4 @@
 import { NgHttpCachingStorageInterface } from './ng-http-caching-storage.interface';
 import { NgHttpCachingEntry } from '../ng-http-caching.service';
 
-export class NgHttpCachingMemoryStorage implements NgHttpCachingStorageInterface {
-
-    get size(): number {
-        return this.store.size;
-    }
-
-    private store = new Map<string, NgHttpCachingEntry<any, any>>();
-
-    clear(): void {
-        this.store.clear();
-    }
-
-    delete(key: string): boolean {
-        return this.store.delete(key);
-    }
-
-    forEach<K, T>(callbackfn: (value: NgHttpCachingEntry<K, T>, key: string) => void): void {
-        this.store.forEach(callbackfn);
-    }
-
-    get<K, T>(key: string): Readonly<NgHttpCachingEntry<K, T>> | undefined {
-        return this.store.get(key);
-    }
-
-    has(key: string): boolean {
-        return this.store.has(key);
-    }
-
-    set<K, T>(key: string, value: NgHttpCachingEntry<K, T>): void {
-        this.store.set(key, value);
-    }
-}
+export class NgHttpCachingMemoryStorage extends Map<string, NgHttpCachingEntry<any, any>> implements NgHttpCachingStorageInterface { }
